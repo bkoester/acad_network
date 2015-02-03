135f64504816b973b4cff4d5bf20aea5c81795cd
Kar Epker <karepker+services@gmail.com>
2015-02-03 00:06:46 -0500

    Switched implementation to use an adjacency matrix. Should be faster.

---
+++ src/adj_mat_serialize.hpp
@@ -0,0 +1,109 @@
+#ifndef ADJ_MAT_SERIALIZE_HPP
+#define ADJ_MAT_SERIALIZE_HPP
+
+#include <map>
+#include <utility>
+
+#include <boost/graph/adjacency_matrix.hpp>
+#include <boost/graph/iteration_macros.hpp>
+#include <boost/pending/property_serialize.hpp>
+#include <boost/config.hpp>
+#include <boost/detail/workaround.hpp>
+
+#include <boost/serialization/collections_save_imp.hpp>
+#include <boost/serialization/collections_load_imp.hpp>
+#include <boost/serialization/split_free.hpp>
+
+/* This file heavily inspired by the included boost/graph/adj_list_serialize.hpp
+ * file with some changes for my own style and to make loading work with an
+ * adjacency matrix, which is not default constructible. */
+
+namespace boost { 
+
+namespace serialization {
+
+template<typename Archive, typename D, typename VP, typename EP, typename GP, 
+	typename EL>
+inline void save(Archive & ar,
+		const boost::adjacency_matrix<D, VP, EP, GP, EL>& graph,
+		const unsigned int /* file_version */) {
+	typedef boost::adjacency_matrix<D, VP, EP, GP, EL> Graph;
+	typedef typename graph_traits<Graph>::vertex_descriptor Vertex;
+
+	int V = num_vertices(graph);
+	int E = num_edges(graph);
+	ar << BOOST_SERIALIZATION_NVP(V);
+	ar << BOOST_SERIALIZATION_NVP(E);
+
+	// assign indices to vertices
+	std::map<Vertex, long> indices;
+	long num{0};
+	BGL_FORALL_VERTICES_T(v, graph, Graph) {
+		indices[v] = num++;
+		ar << serialization::make_nvp(
+				"vertex_property", get(vertex_all_t(), graph, v) );
+	}
+
+	// write edges
+	BGL_FORALL_EDGES_T(e, graph, Graph) {
+		ar << serialization::make_nvp("u" , indices[source(e,graph)]);
+		ar << serialization::make_nvp("v" , indices[target(e,graph)]);
+		ar << serialization::make_nvp("edge_property", get(edge_all_t(), graph, e) );
+	}
+}
+
+
+template <typename Archive, typename D, typename VP, typename EP, typename GP, 
+		 typename EL>
+inline void load(Archive & ar,
+				 boost::adjacency_matrix<D, VP, EP, GP, EL>& graph,
+				 const unsigned int /* file_version */) {
+	typedef boost::adjacency_matrix<D, VP, EP, GP, EL> Graph;
+	typedef typename graph_traits<Graph>::vertex_descriptor Vertex;
+	typedef typename graph_traits<Graph>::edge_descriptor Edge;
+
+	long unsigned V;
+	ar >> BOOST_SERIALIZATION_NVP(V);
+	long unsigned E;
+	ar >> BOOST_SERIALIZATION_NVP(E);
+	// create temporary graph object to hold values because we can't add
+	// vertices to a constructed adjacency matrix
+	Graph tmp{V};
+
+	// read vertices
+	std::vector<Vertex> verts(V);
+	long index{0};
+	while (V-- > 0){
+		Vertex v{vertex(index, tmp)};
+		verts[index++] = v;
+		ar >> serialization::make_nvp(
+				"vertex_property", get(vertex_all_t(), tmp, v));
+	}
+
+	// read edges
+	while (E-- > 0){
+		long u; long v;
+		ar >> BOOST_SERIALIZATION_NVP(u);
+		ar >> BOOST_SERIALIZATION_NVP(v);
+		Edge e{add_edge(verts[u], verts[v], tmp).first};
+		ar >> serialization::make_nvp(
+				"edge_property", get(edge_all_t(), tmp, e));
+	}
+
+	// assign the temporary graph object to the input graph object
+	graph = std::move(tmp);
+}
+
+template <typename Archive, typename D, typename VP, typename EP, typename GP, 
+		 typename EL>
+inline void serialize(Archive & ar,
+		boost::adjacency_matrix<D, VP, EP, GP, EL>& graph,
+		const unsigned int file_version){
+    boost::serialization::split_free(ar, graph, file_version);
+}
+
+} // serialization
+} // boost
+
+
+#endif  // ADJ_MAT_SERIALIZE_HPP
--- src/student_network_test.cpp
+++ src/student_network_test.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <sstream>
 
-#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/adjacency_matrix.hpp>
 #include "gtest/gtest.h"
 
 #include "course.hpp"
@@ -18,37 +18,40 @@ using std::find_if;
 using std::stringstream;
 
 using boost::add_edge;
-using boost::add_vertex;
+using boost::vertex;
 
 
 class StudentNetworkTest : public ::testing::Test {
  public:
 	 void SetUp() override {
+		 StudentNetwork::graph_t graph{5};
+
 		 // add vertices
-		 auto student1 = add_vertex(StudentId{312995}, graph_);
-		 auto student2 = add_vertex(StudentId{500928}, graph_);
-		 auto student3 = add_vertex(StudentId{147195}, graph_);
-		 auto student4 = add_vertex(StudentId{352468}, graph_);
-		 auto student5 = add_vertex(StudentId{567890}, graph_);
+		 auto student1 = vertex(0, graph);
+		 graph[student1] = StudentId{312995};
+ 		 auto student2 = vertex(1, graph);
+		 graph[student2] = StudentId{500928};
+ 		 auto student3 = vertex(2, graph);
+		 graph[student3] = StudentId{147195};
+ 		 auto student4 = vertex(3, graph);
+		 graph[student4] = StudentId{352468};
+ 		 auto student5 = vertex(4, graph);
+		 graph[student5] = StudentId{567890};
 
 		 // add edges
-		 add_edge(student1, student2, 1, graph_);
-		 add_edge(student1, student3, 1, graph_);
-		 add_edge(student1, student4, 1, graph_);
+		 add_edge(student1, student2, 1, graph);
+		 add_edge(student1, student3, 1, graph);
+		 add_edge(student1, student4, 1, graph);
 
-		 add_edge(student2, student3, 1, graph_);
-		 add_edge(student2, student4, 1, graph_);
+		 add_edge(student2, student3, 1, graph);
+		 add_edge(student2, student4, 1, graph);
 
-		 add_edge(student3, student4, 1, graph_);
-		 add_edge(student4, student5, 1, graph_);
+		 add_edge(student3, student4, 1, graph);
+		 add_edge(student4, student5, 1, graph);
 
-		 network = StudentNetwork{graph_};
+		 network = StudentNetwork{graph};
 	 }
 
- private:
-	StudentNetwork::graph_t graph_;
-
-   // must be defined after the private member for initialization reasons
  protected:
 	StudentNetwork network;
 };

fa6b92c07479b479e1711ef6f30ea465f4ceed32
Kar Epker <karepker+services@gmail.com>
2015-07-20 23:54:44 -0400

    Allowed script to accept arbitrary number of input files.

--- simpson_diversity.py
+++ simpson_diversity.py
@@ -14,9 +14,12 @@ __copyright__ = "Kar Epker, 2015"
 
 
 import argparse
+import sys
+
+import numpy
+
 from readable_directory import ReadableDirectory
 from student_container_wrapper import StudentContainerWrapper
-import sys
 import vertex_analysis
 
 
@@ -29,8 +32,8 @@ if __name__ == '__main__':
     parser.add_argument('weightedness', choices=['weighted', 'unweighted'],
             help='Calculate using weights of connections or just by frequency '
             'of connections.')
-    parser.add_argument('file', nargs='?', type=argparse.FileType('r'),
-            default=sys.stdin, help='File to parse (default standard input).')
+    parser.add_argument('-f', '--files', nargs='*', type=argparse.FileType('r'),
+            default=sys.stdin, help='Files to parse (default standard input).')
     parser.add_argument('--student-archive-path', dest='student_archive_path',
             help='Path of the student archive file.', required=True)
     parser.add_argument('--swig-module-path', dest='swig_module_path',
@@ -43,24 +46,29 @@ if __name__ == '__main__':
     students_wrapper = StudentContainerWrapper(
             args.swig_module_path, args.student_archive_path)
     segmenter = StudentContainerWrapper.SEGMENTERS[args.segmenter]
-    vertex_lines = vertex_analysis.parse_vertex_file(args.file)
 
-    segmented_lines = list(vertex_analysis.map_to_segments(
-            vertex_lines, segmenter, students_wrapper))
-    total_connections = len(segmented_lines)
-    values_sum = vertex_analysis.sum_values(segmented_lines)
-    segments = vertex_analysis.reduce_to_segments(segmented_lines)
+    for vertex_file in args.files:
+        vertex_lines = vertex_analysis.parse_vertex_file(vertex_file)
 
-    # calculate the diversity of the segments
-    diversity_index = 0
-    for values in segments.values():
-        # use only the number of values in the segment if unweighted
-        if args.weightedness == 'unweighted':
-            diversity_index += (values.size / total_connections) ** 2
-        # use the sum of the values in the segment if weighted
-        elif args.weightedness == 'weighted':
-            diversity_index += (numpy.sum(values) / values_sum) ** 2
+        segmented_lines = list(vertex_analysis.map_to_segments(
+                vertex_lines, segmenter, students_wrapper))
+        total_connections = len(segmented_lines)
+        values_sum = vertex_analysis.sum_values(segmented_lines)
+        segments = vertex_analysis.reduce_to_segments(segmented_lines)
+
+        # calculate the diversity of the segments
+        diversity_index = 0
+        for values in segments.values():
+            # use only the number of values in the segment if unweighted
+            if args.weightedness == 'unweighted':
+                diversity_index += (values.size / total_connections) ** 2
+            # use the sum of the values in the segment if weighted
+            elif args.weightedness == 'weighted':
+                diversity_index += (numpy.sum(values) / values_sum) ** 2
+            else:
+                raise ValueError('Unknown argument for weightedness given!')
+
+        if len(args.files) > 1:
+            print('{}\t{}'.format(vertex_file.name, diversity_index))
         else:
-            raise ValueError('Unknown argument for weightedness given!')
-
-    print(diversity_index)
+            print(diversity_index)

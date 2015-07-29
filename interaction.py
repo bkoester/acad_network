8ea364c97ca195027051dce14a52dcd42182c643
Kar Epker <karepker@gmail.com>
2015-07-29 19:16:28 -0400

    Updated interaction to use cleaned up coding structure.

--- interaction.py
+++ interaction.py
@@ -1,4 +1,4 @@
-"""Performs a basic reading of data from a tsv."""
+"""Compares RPD of actual vs. expected interactions of nodes in a network."""
 
 
 __author__ = "karepker@umich.edu Kar Epker"
@@ -6,123 +6,98 @@ __copyright__ = "Kar Epker, 2015"
 
 
 import argparse
-import collections
-import csv
 import math
-import matplotlib.pyplot
-import numpy
 import sys
 
-import data_processing
-import student_analysis
+import matplotlib.pyplot
+import numpy
 
-
-def get_vertex_total_weight(edges, vertex):
-    """Gets the total edge weight outgoing from any vertex.
-
-    Args:
-        edges (pair => int): The map of edges.
-
-    Returns:
-        The total outgoing weight from a vertex
-    """
-    total_weight = 0
-    for vertices, weight in edges.items():
-        if vertices[0] == vertex or vertices[1] == vertex:
-            total_weight += weight
-
-    return total_weight
-
-
-def relative_percent_difference(value1, value2):
-    """Calculates relative percent difference between two values.
-    
-    Args:
-        value1 (number)
-        value2 (number)
-
-    Returns:
-        The relative percent difference between the two values.
-    """
-    difference = value1 - value2
-    value_sum = value1 + value2
-    rpd = 2 * difference / value_sum
-    return rpd
+import calculation
+import edge_analysis
+from readable_directory import ReadableDirectory
+from student_container_wrapper import StudentContainerWrapper
+import segment
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Analyze data dealing with'
-        'networks.')
-    parser.add_argument('-f', '--student-file', dest='student_path', 
-            default='../data/student.tab',
-            help='The file containing edge interactions.')
-    parser.add_argument('field', 
-        help='The field for which to analyze interactions.')
-    parser.add_argument('weightedness', 
-        help='Either "weighted" or "unweighted"')
-        
+    parser = argparse.ArgumentParser(
+            description='Show RPD between actual and expected interactions of '
+            'segments.')
+    parser.add_argument(
+            'field', help='The field for which to analyze interactions.')
+    parser.add_argument(
+            'weightedness', choices=['weighted', 'unweighted'],
+            help='Either "weighted" or "unweighted".')
+    parser.add_argument(
+            'file', nargs='?', type=argparse.FileType('r'),
+            default=sys.stdin, help='File to segment (default standard input).')
+    parser.add_argument(
+            '--student-archive-path', dest='student_archive_path',
+            help='Path of the student archive file.', required=True)
+    parser.add_argument(
+            '--swig-module-path', dest='swig_module_path',
+            help='Directory containing the swig modules.',
+            action=ReadableDirectory, required=True)
+
     args = parser.parse_args()
 
-    if args.weightedness not in ['weighted', 'unweighted']:
-        raise ValueError('Weightedness %s not known!' % args.weightedness)
-
-    # get actual interaction values
-    actual_edges = data_processing.get_edges(sys.stdin)
+    # read edges from file; these correspond to actual interaction values
+    edge_lines = list(edge_analysis.get_vertices_values(args.file))
 
     # get expected interaction values
-    with open(args.student_path, 'r') as student_file:
-        segment_counts = student_analysis.get_segment_counts(
-                student_file, args.field)
-         
+    students_wrapper = StudentContainerWrapper(
+            args.swig_module_path, args.student_archive_path)
+    segment_counts = segment.get_segment_counts(students_wrapper, args.field)
+
     # compare actual edges with expected edges, put into matrix
     segments = sorted(list(segment_counts.keys()))
     population = sum(segment_counts.values())
     heatmatrix = numpy.zeros((len(segments), len(segments)))
-    for vertices_pair, actual_weight in actual_edges.items():
-        vertex1 = vertices_pair[0]
-        vertex2 = vertices_pair[1]
+    for vertex1, vertex2, actual_weight in edge_lines:
         vertex1_weight = segment_counts[vertex1]
         vertex2_weight = segment_counts[vertex2]
 
         # store vertex expected weight
-        v1v2_expected_weight = ((vertex2_weight / population) *
-                get_vertex_total_weight(actual_edges, vertex1))
-        v2v1_expected_weight = ((vertex1_weight / population) *
-                get_vertex_total_weight(actual_edges, vertex2))
+        v1v2_expected_weight = (
+                (vertex2_weight / population) *
+                edge_analysis.get_vertex_total_weight(edge_lines, vertex1))
+        v2v1_expected_weight = (
+                (vertex1_weight / population) *
+                edge_analysis.get_vertex_total_weight(edge_lines, vertex2))
 
         # store the difference of actual and expected in a matrix
         vertex1_index = segments.index(vertex1)
         vertex2_index = segments.index(vertex2)
-        heatmatrix[vertex2_index, vertex1_index] = relative_percent_difference(
-                actual_weight, v1v2_expected_weight)
+        heatmatrix[vertex2_index, vertex1_index] = (
+                calculation.relative_percent_difference(
+                        actual_weight, v1v2_expected_weight))
 
         if vertex1 != vertex2:
             heatmatrix[vertex1_index, vertex2_index] = (
-                    relative_percent_difference(
-                        actual_weight, v2v1_expected_weight))
-            
+                    calculation.relative_percent_difference(
+                            actual_weight, v2v1_expected_weight))
 
     if len(segments) > 50:
-        # print out just segment RPDS with themselves
+        # print out just segment RPDs with themselves
         segment_self_rpds = dict()
         for i in range(0, heatmatrix.shape[0]):
             segment_self_rpds[segments[i]] = heatmatrix[i, i]
 
         # sort them
-        sorted_segment_self_rpds = sorted([(segment, self_rpd) for 
-                (segment, self_rpd) in segment_self_rpds.items()],
-            key=lambda x: x[1], reverse=True)
+        sorted_segment_self_rpds = sorted(
+                [(segment, self_rpd) for (segment, self_rpd)
+                 in segment_self_rpds.items()],
+                key=lambda x: x[1], reverse=True)
 
         for segment, self_rpd in sorted_segment_self_rpds:
-            print("%s || %.2f" % (segment, self_rpd))
+            print('{}\t{}'.format(segment, self_rpd))
 
     else:
-        figsize = (3 + math.ceil(len(segments) * 0.6), 
-                       2 + math.ceil(len(segments) * 0.4))
+        figsize = (3 + math.ceil(len(segments) * 0.6),
+                   2 + math.ceil(len(segments) * 0.4))
         fig = matplotlib.pyplot.figure(figsize=figsize)
         ax = fig.add_subplot(1, 1, 1)
-        heatmap = matplotlib.pyplot.pcolor(
-                heatmatrix, cmap=matplotlib.pyplot.cm.RdBu)
+        heatmap = matplotlib.pyplot.pcolor(heatmatrix, cmap='RdBu')
         heatmap.set_clim(vmin=-1.0, vmax=1.0)
 
         # where the ticks are located
@@ -132,6 +107,11 @@ if __name__ == '__main__':
         # tick locations relative to the cell
         ax.set_xticks(numpy.arange(heatmatrix.shape[0]) + 0.5, minor=False)
         ax.set_yticks(numpy.arange(heatmatrix.shape[1]) + 0.5, minor=False)
+        matplotlib.pyplot.tick_params(
+                axis='x', which='both', bottom='off', top='off')
+        matplotlib.pyplot.tick_params(
+                axis='y', which='both', right='off', left='off')
+
         # stop a white bar from appearing due to rounding
         ax.set_xlim(right=len(segments))
         ax.set_ylim(bottom=len(segments))
@@ -144,8 +124,8 @@ if __name__ == '__main__':
         matplotlib.pyplot.xticks(rotation=90)
 
         # labels
-        matplotlib.pyplot.xlabel('%s interactions from ...' %
-                (args.weightedness.title()))
+        matplotlib.pyplot.xlabel(
+                '{} interactions from ...'.format(args.weightedness.title()))
         ax.xaxis.set_label_position('top')
         matplotlib.pyplot.ylabel('To ...')
 

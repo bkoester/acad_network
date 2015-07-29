44770df30aac2dfce4beccb987ae7c3f7f5ae7fc
Kar Epker <karepker@gmail.com>
2015-07-29 19:35:23 -0400

    Cleaned up and brought ks_test module up to date.

--- ks_test.py
+++ ks_test.py
@@ -13,25 +13,54 @@ import numpy
 import scipy.stats
 import sys
 
-import data_processing
+from readable_directory import ReadableDirectory
+from student_container_wrapper import StudentContainerWrapper
+import vertex_analysis
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Analyze data dealing with'
-        'networks.')
-    parser.add_argument('weightedness', 
-        help='Either "weighted" or "unweighted"')
-    parser.add_argument('--threshold', type=int, default=100,
+    parser = argparse.ArgumentParser(
+            description='Perform KS tests on all pairs of segments.')
+    parser.add_argument(
+            'weightedness', choices=['weighted', 'unweighted'],
+            help='Either "weighted" or "unweighted".')
+    parser.add_argument(
+            'file', nargs='?', type=argparse.FileType('r'),
+            default=sys.stdin, help='File to segment containing data to put in '
+            'on which to run KS tests.')
+    parser.add_argument(
+            '-s', '--segmenter', default='all',
+            choices=StudentContainerWrapper.SEGMENTERS.keys(),
+            help='Segmenting function to use.')
+    parser.add_argument(
+            '--threshold', type=int, default=100,
             help='Filter segments with fewer than this many data points.')
- 
+    parser.add_argument(
+            '--student-archive-path', dest='student_archive_path',
+            help='Path of the student archive file.', required=True)
+    parser.add_argument(
+            '--swig-module-path', dest='swig_module_path',
+            help='Directory containing the swig modules.',
+            action=ReadableDirectory, required=True)
+
     args = parser.parse_args()
 
-    # put all the points from the distributions into a numpy array
-    segments_kv = data_processing.segment_data(sys.stdin, True)
-    filtered_segments = {segment: values for segment, values in 
-            segments_kv.items() if values.size >= args.threshold}
+    # segment the data
+    vertex_lines = vertex_analysis.get_id_values(args.file)
+    students_wrapper = StudentContainerWrapper(
+            args.swig_module_path, args.student_archive_path)
+    segmenter = StudentContainerWrapper.SEGMENTERS[args.segmenter]
+    mapped_lines = vertex_analysis.map_to_segments(
+            vertex_lines, segmenter, students_wrapper)
+    unfiltered_segments = vertex_analysis.reduce_to_segments(mapped_lines)
+
+    # filter out small segments
+    filtered_segments = {segment: values
+                         for segment, values in unfiltered_segments.items()
+                         if values.size >= args.threshold}
     segments = sorted(filtered_segments.keys())
 
+    # put all the points from the distributions into a numpy array
     heatmatrix = numpy.zeros((len(segments), len(segments)))
     pvalue = numpy.zeros((len(segments), len(segments)))
     cells = {}
@@ -52,15 +81,16 @@ if __name__ == '__main__':
         heatmatrix[segment2_index, segment1_index] = ks_stat
 
         cells[(segment1, segment2)] = ks_stat
-        
+
 
     # don't show the plot if there are too many segments. Instead, print out
-    # interesting KV values
+    # KS values
     if len(segments) > 50:
-        sorted_segments = sorted([(segments[0], segments[1], ks_stat) for 
-                segments, ks_stat in cells.items()], key=lambda x: x[2],
-                reverse=True)
-        for sorted_segment in sorted_segments[:10]:
+        sorted_segments = sorted(
+                [(segments[0], segments[1], ks_stat)
+                 for segments, ks_stat in cells.items()],
+                key=lambda x: x[2], reverse=True)
+        for sorted_segment in sorted_segments:
             segment1 = sorted_segment[0]
             segment2 = sorted_segment[1]
             ks_stat = sorted_segment[2]
@@ -68,13 +98,13 @@ if __name__ == '__main__':
             segment1_index = segments.index(segment1)
             segment2_index = segments.index(segment2)
 
-            print("%s || %s || %.2f with p-value: %.2f" % (
-                segment1, segment2, ks_stat, 
-                pvalue[segment1_index, segment2_index]))
+            print('{} || {} || {:2f} with p-value: {:2f}'.format(
+                    segment1, segment2, ks_stat,
+                    pvalue[segment1_index, segment2_index]))
 
 
     else:
-        figsize = (3 + math.ceil(len(segments) * 0.6), 
+        figsize = (3 + math.ceil(len(segments) * 0.6),
                    2 + math.ceil(len(segments) * 0.4))
         fig = matplotlib.pyplot.figure(figsize=figsize)
         ax = fig.add_subplot(1, 1, 1)
@@ -96,19 +126,24 @@ if __name__ == '__main__':
         # tick labels
         ax.set_xticklabels(segments, minor=False)
         ax.set_yticklabels(segments, minor=False)
+        matplotlib.pyplot.tick_params(
+                axis='x', which='both', bottom='off', top='off')
+        matplotlib.pyplot.tick_params(
+                axis='y', which='both', right='off', left='off')
 
         colorbar = matplotlib.pyplot.colorbar(heatmap)
-        #colorbar.set_ticklabels([i for i in numpy.linspace(
-        #    0, 1.0, min(2, 10))])
-        colorbar.set_ticklabels([0, 0.5, 1.0])
+        colorbar.set_ticklabels([i for i in numpy.linspace(
+                0, 1.0, 3 if len(segments) < 10 else 11)])
+        colorbar.set_ticks([i for i in numpy.linspace(
+                0, 1.0, 3 if len(segments) < 10 else 11)])
 
         # rotation of x-axis
         matplotlib.pyplot.xticks(rotation=90)
 
         # labels
         matplotlib.pyplot.xlabel(
-                '%s degree distribution KS statistic between ...' %
-                (args.weightedness.title()))
+                '{} degree distribution KS statistic between ...'.format(
+                        args.weightedness.title()))
         ax.xaxis.set_label_position('top')
         matplotlib.pyplot.ylabel('and ...')
 
@@ -117,7 +152,8 @@ if __name__ == '__main__':
         for y in range(heatmatrix.shape[0]):
             for x in range(heatmatrix.shape[1]):
                 color = 'black' if abs(heatmatrix[y, x]) < 0.5 else 'white'
-                text = '%.2f' % heatmatrix[y, x] if pvalue[y, x] <= 0.05 else '-'
+                text = ('%.2f' % heatmatrix[y, x] if pvalue[y, x] <= 0.05
+                        else '-')
                 matplotlib.pyplot.text(
                         x + 0.5, y + 0.5, text,
                         horizontalalignment='center',
